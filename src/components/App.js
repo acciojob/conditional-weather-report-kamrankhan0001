96cc004c4c240843ad4689e5300f555edec4585a
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-26 01:21:22 +0530

    submitting code-Wed Jul 26 2023 01:21:20 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,13 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+const weatherData = { temperature: 25, conditions: "Sunny" };
+const threshold = 20;
   return (
-    <div>
-        {/* Do not remove the main div */}
+     <div className="App">
+      <h2>Weather for Your Location</h2>
+      <p>Temperature: <span style={{ color: weatherData.temperature > threshold ? 'red' : 'blue' }}>{weatherData.temperature}°C</span></p>
+      <p>Conditions: {weatherData.conditions}</p>
     </div>
   )
 }
